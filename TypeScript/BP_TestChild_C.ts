a16ecebad04d96166042b3ea1cbd9482bd44de67
Edenlia <zsy010906@gmail.com>
2026-05-13 10:25:14 +0800

    复现继承mixin类bug

---
+++ TypeScript/BP_TestChild_C.ts
@@ -0,0 +1,23 @@
+﻿import * as UE from 'ue'
+import {argv, blueprint} from 'puerts';
+import {BP_TestParent_TS, BP_TestParent} from './BP_TestParent_C';
+
+let BP_TestChild_UClass = UE.Class.Load('/Game/Blueprints/BP_TestChild.BP_TestChild_C');
+const BP_TestChild_JsProxyClass = blueprint.tojs<typeof UE.Game.Blueprints.BP_TestChild.BP_TestChild_C>(BP_TestChild_UClass);
+interface BP_TestChild_TS extends UE.Game.Blueprints.BP_TestChild.BP_TestChild_C {}
+
+class BP_TestChild_TS extends BP_TestParent_TS {
+    ReceiveBeginPlay():void {
+        // super.ReceiveBeginPlay();
+        console.log(`BP_TestChild Ts ReceiveBeginPlay`);
+    }
+
+    // ReceiveTick(DeltaSeconds: number) {
+    //     // console.log(`BP_TestChild Ts ReceiveTick`);
+    // }
+}
+
+const BP_TestChild = blueprint.mixin(BP_TestChild_JsProxyClass, BP_TestChild_TS);
+
+export {BP_TestChild_TS, BP_TestChild};
+export default BP_TestChild;
---
+++ TypeScript/BP_TestParent_C.ts
@@ -0,0 +1,21 @@
+﻿import * as UE from 'ue'
+import {argv, blueprint} from 'puerts';
+
+let BP_TestParent_UClass = UE.Class.Load('/Game/Blueprints/BP_TestParent.BP_TestParent_C');
+const BP_TestParent_JsProxyClass = blueprint.tojs<typeof UE.Game.Blueprints.BP_TestParent.BP_TestParent_C>(BP_TestParent_UClass);
+interface BP_TestParent_TS extends UE.Game.Blueprints.BP_TestParent.BP_TestParent_C {}
+
+class BP_TestParent_TS {
+    ReceiveBeginPlay():void {
+        console.log(`BP_TestParent Ts ReceiveBeginPlay`);
+    }
+
+    ReceiveTick(DeltaSeconds: number) {
+        // console.log(`BP_TestParent Ts ReceiveTick`);
+    }
+}
+
+const BP_TestParent = blueprint.mixin(BP_TestParent_JsProxyClass, BP_TestParent_TS);
+
+export {BP_TestParent_TS, BP_TestParent};
+export default BP_TestParent;
